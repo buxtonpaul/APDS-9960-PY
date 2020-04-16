4de60e9b2083cbddd9f9ab7d2474a482b441f9e3
Michael <michalp2404@onet.pl>
2020-04-16 22:28:09 +0200

    Ported to python3

--- gesture_test.py
+++ gesture_test.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 # Example using a character LCD connected to a Raspberry Pi or BeagleBone Black.
 import time
@@ -7,57 +6,48 @@ import apds9960 as GestureSensor
 from apds9960_constants import *
 import RPi.GPIO as GPIO  
 
-
-
 SENSOR_INTERRUPT = 4
 
 def intcallback(channel):
-    #print "Callback"
     while sensor.isGestureAvailable():
         motion=sensor.readGesture()
         if motion == Directions.DIR_NONE:
-            print "None"
+            print("None")
         if motion == Directions.DIR_LEFT:
-            print "Left"
+            print("Left")
         if motion == Directions.DIR_RIGHT:
-            print "Right"
+            print("Right")
         if motion == Directions.DIR_UP:
-            print "Up"
+            print("Up")
         if motion == Directions.DIR_DOWN:
-            print "Down"
+            print("Down")
         if motion == Directions.DIR_NEAR:
-            print "Near"
+            print("Near")
         if motion == Directions.DIR_FAR:
-            print "Far"
+            print("Far")
         sensor.enableGestureSensor(True)
-
 
 sensor = GestureSensor.APDS9960(bus=1)
 
 sensor.initDevice()
 sensor.resetGestureParameters()
 
-
-# These values work better for me than the defaults, perhaps because I am not using the Adafruit one...
 sensor.setGestureGain(GGAIN_2X)
 sensor.setGestureLEDDrive(LED_DRIVE_25MA)
   
 sensor.enableGestureSensor(True)
 
-#sensor.clearGestureInt()
 time.sleep(0.5)
 
-raw_input("Press Enter when ready\n>")
+input("Press Enter when ready\n>")
 
 GPIO.setmode(GPIO.BCM)
 
-# GPIO 4 set up as an input, pulled up, pulled down on interrupt
 GPIO.setup(SENSOR_INTERRUPT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(SENSOR_INTERRUPT, GPIO.FALLING, callback=intcallback)
 
 if sensor.isGestureAvailable():
     sensor.readGesture()
-
 
 try:
     while True:
@@ -70,7 +60,7 @@ try:
         #print "Light Colors({},{},{})".format(red,green,blue)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+    GPIO.cleanup()
 
 sensor.resetGestureParameters()
-print "Done"
+print("Done")
